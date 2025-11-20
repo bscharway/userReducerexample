fc99b1df50067f39e5164ea26fe25be2b7c35602
bscharway <charway30@gmail.com>
2025-11-20 11:25:57 +0000

    complex object used with reducer

--- useReducerTutorial/src/App.jsx
+++ useReducerTutorial/src/App.jsx
@@ -2,21 +2,27 @@ import { useReducer } from 'react'
 import './App.css'
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {count: 0})
-  function reducer (state, action){
-    if (action.type == "increment"){
-      return {...state, count: state.count + action.payload} //state + action.payload;
+  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 2 })
+  function reducer(state, action) {
+    if (action.type == "increment") {
+      return { ...state, count: state.count + state.incrementBy } //state + action.payload;
     }
-    if (action.type == "decrement"){
-      return {...state, count: state.count - action.payload}//state - action.payload;
+    if (action.type == "decrement") {
+      return { ...state, count: state.count - state.incrementBy }//state - action.payload;
+    }
+    if (action.type == "setIncrement"){
+
+      return {...state, incrementBy: action.payload}
+      
     }
   }
   return (
     <div>
-    {state.count}
-    <button onClick={()=> dispatch ({type: "increment", payload: 1})}>Increment count</button>
-    <button onClick={()=> dispatch ({type: "decrement", payload: 1})}>Decrement count</button>
-      
+      <input value={state.incrementBy} onChange={(e)=>dispatch({type:"setIncrement", payload:Number(e.target.value)})}></input>
+      {state.count}
+      <button onClick={() => dispatch({ type: "increment"})}>Increment count</button>
+      <button onClick={() => dispatch({ type: "decrement"})}>Decrement count</button>
+
     </div>
   )
 }
