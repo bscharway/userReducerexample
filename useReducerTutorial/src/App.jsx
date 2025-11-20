cf4c284626fabe2811e4c285afbec7f942fdecf7
bscharway <charway30@gmail.com>
2025-11-20 14:07:03 +0000

    creating routes

--- useReducerTutorial/src/App.jsx
+++ useReducerTutorial/src/App.jsx
@@ -1,49 +1,18 @@
-import { useReducer, useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Homepage from './pages/Homepage';
+import Dashboard from "./pages/Dashboard";
+import PageNotfound from "./pages/PageNotFound";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { balance: 0, incrementBy: 1 })
-  const [amount, setAmount] = useState(0);
-
-  function reducer(state, action){
-    if (action.type== "add"){
-      return {...state, balance:state.balance+action.payload}
-    }
-    if (action.type == "sub"){
-      return {...state, balance: state.balance - action.payload}
-    }
-  }
-  /*function reducer(state, action) {
-    if (action.type == "increment") {
-      return { ...state, count: state.count + state.incrementBy } //state + action.payload;
-    }
-    if (action.type == "decrement") {
-      return { ...state, count: state.count - state.incrementBy }//state - action.payload;
-    }
-    if (action.type == "setIncrement") {
-
-      return { ...state, incrementBy: action.payload }
-
-    }
-  }*/
   return (
-    <div>
-      {state.balance}
-      <input type='text' onChange={(e) => setAmount(e.target.value)} value={amount} name='' id=''></input>
-      <button onClick={() => dispatch({ type: "add", payload: Number(amount) })}>Deposit</button>
-      <button onClick={() => dispatch({ type: "sub", payload: Number(amount) })}>Withdrag</button>
-
-    </div>
-  )
-  /*return (
-    <div>
-      <input type='text' value={state.incrementBy} onChange={(e)=>dispatch({type:"setIncrement", payload:Number(e.target.value)})}></input>
-      {state.count}
-      <button onClick={() => dispatch({ type: "increment"})}>Increment count</button>
-      <button onClick={() => dispatch({ type: "decrement"})}>Decrement count</button>
-
-    </div>
-  )*/
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='*' element={<PageNotfound />} />
+      </Routes>
+    </BrowserRouter>)
 }
 
 export default App
---
+++ useReducerTutorial/src/pages/PageNotFound.jsx
@@ -0,0 +1,3 @@
+export default function PageNotfound(){
+    return <div>Page not found</div>
+}
