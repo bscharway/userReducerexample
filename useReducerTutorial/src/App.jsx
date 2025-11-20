71775376f92a29a16f295034cf299dcb320b806e
bscharway <charway30@gmail.com>
2025-11-20 13:36:52 +0000

    understanding reducer using a Bank account example

--- useReducerTutorial/src/App.jsx
+++ useReducerTutorial/src/App.jsx
@@ -1,30 +1,49 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 2 })
-  function reducer(state, action) {
+  const [state, dispatch] = useReducer(reducer, { balance: 0, incrementBy: 1 })
+  const [amount, setAmount] = useState(0);
+
+  function reducer(state, action){
+    if (action.type== "add"){
+      return {...state, balance:state.balance+action.payload}
+    }
+    if (action.type == "sub"){
+      return {...state, balance: state.balance - action.payload}
+    }
+  }
+  /*function reducer(state, action) {
     if (action.type == "increment") {
       return { ...state, count: state.count + state.incrementBy } //state + action.payload;
     }
     if (action.type == "decrement") {
       return { ...state, count: state.count - state.incrementBy }//state - action.payload;
     }
-    if (action.type == "setIncrement"){
+    if (action.type == "setIncrement") {
 
-      return {...state, incrementBy: action.payload}
-      
+      return { ...state, incrementBy: action.payload }
+
     }
-  }
+  }*/
   return (
     <div>
-      <input value={state.incrementBy} onChange={(e)=>dispatch({type:"setIncrement", payload:Number(e.target.value)})}></input>
+      {state.balance}
+      <input type='text' onChange={(e) => setAmount(e.target.value)} value={amount} name='' id=''></input>
+      <button onClick={() => dispatch({ type: "add", payload: Number(amount) })}>Deposit</button>
+      <button onClick={() => dispatch({ type: "sub", payload: Number(amount) })}>Withdrag</button>
+
+    </div>
+  )
+  /*return (
+    <div>
+      <input type='text' value={state.incrementBy} onChange={(e)=>dispatch({type:"setIncrement", payload:Number(e.target.value)})}></input>
       {state.count}
       <button onClick={() => dispatch({ type: "increment"})}>Increment count</button>
       <button onClick={() => dispatch({ type: "decrement"})}>Decrement count</button>
 
     </div>
-  )
+  )*/
 }
 
 export default App
